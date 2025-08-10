09f05a770590ec6b36feba63075c3a61fe1f5993
lornaegele <lorenz.naegele@mail.de>
2025-08-10 19:50:23 +0200

    changed meta title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { Navbar, ScrollDisabler } from "./components";
 import { mainFont } from "./lib/font";
 
 export const metadata: Metadata = {
-  title: "Lorenz Naegele | Web - App Developer and Videographer",
-  description: `Web and App Developer and Videographer offering innovative digital solutions and compelling visual content. Explore projects, services, and creative work..`,
+  title: "Lorenz Naegele | Filmmaker & Photographer",
+  description: `Filmmaker offering innovative digital solutions and compelling visual content. Explore projects, services, and creative work..`,
   icons: {
     icon: ["/favicon.svg"],
     apple: ["/favicon.svg"],
